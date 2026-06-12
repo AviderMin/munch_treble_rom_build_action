73747ebd3fbd776263be81ddbe3ca91dfc834297
AviderMin <avidermin@gmail.com>
2026-06-12 22:38:37 +0800

    Add Google feature fix logging and permissions setup
    Add echo statement for Google feature fix and create directory for permissions.

--- make.sh
+++ make.sh
@@ -705,6 +705,7 @@ sudo sed -i ''"$(sudo sed -n '/ro.vendor.nfc.repair/=' "$GITHUB_WORKSPACE"/"${de
 sudo find "$GITHUB_WORKSPACE"/"${device}"/vendor/etc/permissions -type f -iname "*beam*.xml" -delete
 
 # 修复 Google 功能
+echo -e "${Red}- 修复 Google 功能"
 sudo mkdir -p "$GITHUB_WORKSPACE"/images/product/etc/permissions
 sudo cp -rf "$GITHUB_WORKSPACE"/"${device}"_files/cn.google.services.xml "$GITHUB_WORKSPACE"/images/product/etc/permissions/
 
